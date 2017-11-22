a478b26281580de0503de2b26d1152d9a32d06c3
ran@panorays.com <ran@panorays.com>
2017-11-22 18:24:40 +0200

    Bump version: 0.5.10 → 0.5.11

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = re.sub(
 
 setup(
     name='bumpversion',
-    version='0.5.10',
+    version='0.5.11',
     url='https://github.com/peritus/bumpversion',
     author='Filip Noetzel',
     author_email='filip+bumpversion@j03.de',
